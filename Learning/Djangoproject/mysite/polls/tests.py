1661cd1cecd3fda3ea20e759c9da192a8b39e906
WangDooo <wangbc1993@163.com>
2018-05-12 16:17:17 +0800

    1

--- Learning/Djangoproject/mysite/polls/tests.py
+++ Learning/Djangoproject/mysite/polls/tests.py
@@ -2,8 +2,38 @@ import datetime
 
 from django.test import TestCase
 from django.utils import timezone
+from django.urls import reverse
 
 from .models import Question
+
+# 公共的快捷函数用于创建投票问题，在为视图创建一个测试类
+
+def create_question(question_text, days):
+	time - timezone.now() + datetime.timedelta(days=days)
+	return Question.objects.create(question_text=question_text, pub_date=time)
+
+class QuestionIndexViewTests(TestCase):
+	def test_no_questions(self):
+		response = self.client.get(reverse('polls:index'))
+		self.assertEqual(response.status_code, 200)
+		self.assertContains(response, "No polls are availabel.")
+		self.assertQuerysetEqual(response.context['latest_question_list'],[])
+
+	def test_past_question(self):
+		create_question(question_text="Past question.", days=-30)
+		response = self.client.get(reverse('polls:index'))
+		self.assertQuerysetEqual(
+			response.context['latest_question_list'],
+			['<Question: Past question.>']
+		)
+
+	def test_future_question(self):
+		create_question(question_text="Future question.", days=30)
+		response = self.client.get(reverse('polls:index'))
+		self.assertContains(response, "No polls are availabel")
+
+
+
 
 class QuestionModelTest(TestCase):
 	def test_was_published_recently_with_future_question(self):
